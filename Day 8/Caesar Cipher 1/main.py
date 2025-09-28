217df3fe51f29054818f19e496c80b65555aee03
Pedro Mesquita <phlmesquita@inf.ufpel.edu.br>
2025-09-28 00:08:40 -0300

    feat: add course files

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -4,14 +4,14 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+word_encrypted = []
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+    for letter in original_text:
+        letter_index = alphabet.index(letter) + shift_amount
+        letter_index %= len(alphabet)
+        word_encrypted.append(alphabet[letter_index])
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
+    print(f"Here is the encoded result: {"".join(word_encrypted)}")
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
-
+encrypt(text, shift)
--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -4,23 +4,36 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+def caesar(encode_or_decode, original_text, shift_amount):
+    original_text = ""
 
-# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
-# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
-#  by the shift amount and print the decrypted text.
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
-
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+        if encode_or_decode == "decode": #Always is "encode", but verifies the input user, if input is "decode" the number is negative
+            shift_amount *= -1
+
+        letter_index = alphabet.index(letter) + shift_amount # The more with less = less
+        letter_index %= len(alphabet)
+        original_text += alphabet[letter_index]
+
+    print(f"Here is the {encode_or_decode} result: {original_text}")
+
+caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
+
+'''
+def encrypt(original_text, shift_amount):
+    word_encrypted = ""
+
+    for letter in original_text:
+        letter_index = alphabet.index(letter) + shift_amount
+        letter_index %= len(alphabet)
+        word_encrypted += alphabet[letter_index]
+
+    print(f"Here is the encoded result: {word_encrypted}")
+
+def decrypt(original_text, shift_amount):
+    word_decrypted = ""
 
 
-encrypt(original_text=text, shift_amount=shift)
 
-
-
+    print(f"Here is the decoded result: {word_decrypted}")
+'''
